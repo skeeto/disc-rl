8324104d7d692c7982443d838396220dddd0f02e
Christopher Wellons <mosquitopsu@gmail.com>
2013-03-09 16:49:15 -0500

    Add sprintf() stuff to the logger.

--- src/log.js
+++ src/log.js
@@ -5,6 +5,7 @@
 var $log = $('#log');
 
 function log(message) {
-    var $message = $('<span/>').attr({'class': 'message'}).text(message);
+    var text = vsprintf(message, Array.prototype.slice.call(arguments, 1));
+    var $message = $('<span/>').attr({'class': 'message'}).text(text);
     $log.prepend($message);
 }
