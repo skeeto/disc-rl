4a17b2dfce15f6e283465dc714400b6a7b81959f
Christopher Wellons <mosquitopsu@gmail.com>
2013-03-09 20:29:36 -0500

    Add debug messages.

--- src/log.js
+++ src/log.js
@@ -2,11 +2,27 @@
  * Exports: log
  */
 
+var DEBUG = true;
+var _slice = Array.prototype.slice;
+
 var $log = $('#log');
 
+function makeTag(text) {
+    var $tag = $('<span/>').attr({'class': 'message'}).text(text);
+    for (var i = 1; i < arguments.length; i++) {
+        $tag.addClass(arguments[i]);
+    }
+    return $tag;
+}
+
 function log(message) {
-    var text = vsprintf(message, Array.prototype.slice.call(arguments, 1));
-    text = capitalize(text);
-    var $message = $('<span/>').attr({'class': 'message'}).text(text);
-    $log.prepend($message);
+    var text = vsprintf(message, _slice.call(arguments, 1));
+    $log.prepend(makeTag(capitalize(text)));
+}
+
+function debug(message) {
+    if (DEBUG) {
+        var text = vsprintf(message, _slice.call(arguments, 1));
+        $log.prepend(makeTag(text, 'debug'));
+    }
 }
