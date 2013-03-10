7011d2f48e9c6b445b6340f1ab7699d3d5a0c4d1
Christopher Wellons <mosquitopsu@gmail.com>
2013-03-09 23:54:10 -0500

    Change the underlying map represenation format.

--- src/map.js
+++ src/map.js
@@ -6,35 +6,22 @@
  * @constructor
  */
 function Map(w, h) {
-    this.grid = [];
+    this.grid = {};
     for (var x = 0; x < w; x++) {
-        var row = [];
-        this.grid.push(row);
         for (var y = 0; y < h; y++) {
-            row.push(new Place());
+            this.grid[[x, y]] = new Place();
         }
     }
     this.visible = {};
 }
 
-Map.prototype.visit = function(f) {
-    for (var x = 0; x < this.grid.length; x++) {
-        for (var y = 0; y < this.grid[x].length; y++) {
-            var place = f(this.grid[x][y], x, y);
-            if (place) {
-                this.grid[x][y] = place;
-            }
-        }
-    }
-    return this;
+Map.prototype.get = function(x, y) {
+    return this.grid[[x, y]];
 };
 
-Map.prototype.get = function(x, y) {
-    if (this.grid[x]) {
-        return this.grid[x][y];
-    } else {
-        return undefined;
-    }
+Map.prototype.set = function(x, y, place) {
+    this.grid[[x, y]] = place;
+    return this;
 };
 
 Map.prototype.isSolid = function(x, y) {
@@ -86,14 +73,16 @@ Map.prototype.display = function() {
 Map.random = function(seed, w, h) {
     var rng = new RNG(seed);
     var types = Array.prototype.slice.call(arguments, 3);
-    var map = new Map(w, h).visit(function(place, x, y) {
-        if (x === 0 || x === w - 1 ||
-            y === 0 || y === h - 1) {
-            return new types[0];
-        } else {
-            return new types[rng.random(types.length)];
+    var map = new Map(w, h);
+    for (var x = 0; x < w; x++) {
+        for (var y = 0; y < h; y++) {
+            if (x === 0 || x === w - 1 || y === 0 || y === h - 1) {
+                map.set(x, y, new types[0]);
+            } else {
+                map.set(x, y, new types[rng.random(types.length)]);
+            }
         }
-    });
+    }
     return map;
 };
 
